d33b114f0b90cd3d0388a1303971920f3e50feef
Adrien Lamoureux <adrien.lamoureux@calmid.com>
2019-07-17 17:20:41 +0900

    Fix tests debugging

--- test/runner_test.go
+++ test/runner_test.go
@@ -3,10 +3,9 @@ package test
 import (
 	"testing"
 
-	"bitbucket.org/calmisland/go-server-shared/v3/configs"
 	"bitbucket.org/calmisland/go-server-product/testproductdatabase/testproductdynamodb"
+	"bitbucket.org/calmisland/go-server-shared/v3/configs"
 	"bitbucket.org/calmisland/product-lambda-funcs/src/server"
-	"github.com/calmisland/go-testify/suite"
 )
 
 // TestProduct execute a defined list of tests, using the project configuration environment
@@ -16,5 +15,5 @@ func TestProduct(t *testing.T) {
 		panic(err)
 	}
 	server.Setup()
-	suite.Run(t, new(testproductdynamodb.ProductTestSuite))
+	testproductdynamodb.RunTestSuite(t)
 }
